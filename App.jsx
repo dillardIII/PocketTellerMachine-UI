4ca06b2ea5c169aeeec7406eee87c766dbd10364
dillardIII <32180783+dillardIII@users.noreply.github.com>
2025-04-05 20:15:49 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="p-6 text-center text-xl font-bold text-white bg-gray-800 min-h-screen">
+      PocketTeller Dashboard (Frontend Starter)
+    </div>
+  );
+}
+
+export default App;
